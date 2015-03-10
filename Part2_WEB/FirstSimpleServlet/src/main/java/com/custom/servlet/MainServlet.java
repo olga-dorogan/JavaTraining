0bb0f06388fd59a13f7eb0f82822ac39e50aec32
Olga Dorogan <dorogan.olga.n@gmail.com>
2015-03-10 21:04:58 +0200

    view is changed

--- Part2_WEB/FirstSimpleServlet/src/main/java/com/custom/servlet/MainServlet.java
+++ Part2_WEB/FirstSimpleServlet/src/main/java/com/custom/servlet/MainServlet.java
@@ -1,76 +1,72 @@
 package com.custom.servlet;
 
+import com.google.gson.Gson;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.Enumeration;
+import java.util.*;
 
 /**
  * Created by olga on 01.03.15.
  */
-@WebServlet("/")
+@WebServlet("/info")
 public class MainServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.setContentType("text/html");
-        PrintWriter writer = resp.getWriter();
-        writer.print("" +
-                "<!DOCTYPE html>\n" +
-                "<html>\n" +
-                "<head lang=\"en\">\n" +
-                "    <meta charset=\"UTF-8\">\n" +
-                "    <title>Example</title>\n" +
-                "</head>\n" +
-                "<body>\n");
+        final String
+                HEADERS = "headers",
+                PARAMS = "params",
+                THREADS = "threads";
+        final String
+                KEY = "key",
+                VALUE = "value";
 
-        writer.print("" +
-                "    <h3>Headers:</h3>\n");
-        writer.print("" +
-                "    <ul>\n");
+        List<Map<String, String>> headers = new ArrayList<Map<String, String>>();
         Enumeration<String> headerNames = req.getHeaderNames();
         while (headerNames.hasMoreElements()) {
-            String headerName = headerNames.nextElement();
-            writer.print("" +
-                    "   <li>" + headerName + " == " + req.getHeader(headerName) + "</li>");
+            Map map = new HashMap<String, String>();
+            String name = headerNames.nextElement();
+            map.put(KEY, name);
+            map.put(VALUE, req.getHeader(name));
+            headers.add(map);
         }
-        writer.print("" +
-                "    </ul>\n");
 
-        writer.print("" +
-                "    <h3>Parameters:</h3>\n");
-        writer.print("" +
-                "    <ul>\n");
-        Enumeration<String> parameterNames = req.getParameterNames();
-        while (parameterNames.hasMoreElements()) {
-            String paramName = parameterNames.nextElement();
-            writer.print("" +
-                    "   <li>" + paramName + " == " + req.getParameter(paramName) + "</li>");
+        List<Map<String, String>> params = new ArrayList<Map<String, String>>();
+        Enumeration<String> paramNames = req.getParameterNames();
+        while (paramNames.hasMoreElements()) {
+            Map map = new HashMap<String, String>();
+            String name = paramNames.nextElement();
+            map.put(KEY, name);
+            map.put(VALUE, req.getParameter(name));
+            params.add(map);
         }
-        writer.print("" +
-                "    </ul>\n");
 
-        writer.print("" +
-                "    <h3>Server threads:</h3>\n");
+        List<Map<String, String>> containerThreads = new ArrayList<Map<String, String>>();
         synchronized (this) {
             int active = Thread.activeCount();
             Thread[] allThreads = new Thread[active];
-            if (Thread.enumerate(allThreads) != active) {
-                writer.print("<p> Error with receiving info about threads");
-            } else {
-                writer.print("<ul>");
+            if (Thread.enumerate(allThreads) == active) {
                 for (int i = 0; i < allThreads.length; i++) {
-                    writer.print("<li>" + allThreads[i].toString() + "</li>");
+                    Map<String, String> map = new HashMap<String, String>();
+                    map.put(KEY, Long.toString(allThreads[i].getId()));
+                    map.put(VALUE, allThreads[i].getName());
+                    containerThreads.add(map);
                 }
-                writer.print("</ul>");
             }
         }
-        writer.print("" +
-                "</body>\n" +
-                "</html>");
+
+        Map<String, List<Map<String, String>>> info = new LinkedHashMap<String, List<Map<String, String>>>();
+        info.put(HEADERS, headers);
+        info.put(PARAMS, params);
+        info.put(THREADS, containerThreads);
+
+        resp.setContentType("application/json");
+        resp.getWriter().print(new Gson().toJson(info));
     }
 }
+
