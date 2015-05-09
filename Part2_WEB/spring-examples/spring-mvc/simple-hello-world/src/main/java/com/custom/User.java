3c9631f6faaf0906bdd77f47de5a1fa9e8d1e314
Olga Dorogan <dorogan.olga.n@gmail.com>
2015-05-09 18:29:34 +0300

    mvc example is added

---
+++ Part2_WEB/spring-examples/spring-mvc/simple-hello-world/src/main/java/com/custom/User.java
@@ -0,0 +1,16 @@
+package com.custom;
+
+/**
+ * Created by olga on 09.05.15.
+ */
+public class User {
+    private String userName;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+}
